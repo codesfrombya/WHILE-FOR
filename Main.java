850d7c251932ac68bb426fd007ec6d1ed5745705
Thamyres <thamy.beatriz05@gmail.com>
2024-10-03 01:24:40 +0000

    Pending changes exported from your codespace

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main{
+    public static void main(String [] args){
+        //While, Do-While
+        //While
+        // lAÇO DE REPETIÇÃO,REPRODUZ A AÇÃO (em loop) ENQUANTO A CONDIÇÃO FOR VERDADEIRA.
+        //exemplo: contador de 0 a 10
+//int contador = 0;
+//while (contador <= 10 ) {
+    //System.out.println(contador);
+   // contador++;
+//} 
+ //OBS: usar contador++; ,  senão o codigo vai entrar em loop infinito
+
+ //Do - While
+ //VARIAÇÃO DO WHILE, executa o código uma vez e depois checa se a condição é verdadeira
+ //exemplo: 0 > 1 = falso
+//int numero = 1;
+//do {
+   // System.out.println(numero);
+    //numero++;
+//} while(numero > 0);
+
+
+//For
+//TAMBEM UM LAÇO, A DIFERENÇA É QUE O FOR VOCÊ USA QUANDO SABE DE ANTEMAO 
+//O TANTO DE VEZES QUE A ÃO TEM QUE SE REPETIR
+//for( variavel inicio, final, o quanto aumenta-diminui)
+//for(int i = 0; i < 10; i += 2){
+ //System.out.println(i);
+//}
+
+}
+}
+
